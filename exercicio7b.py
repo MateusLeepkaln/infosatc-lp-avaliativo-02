c5277b794bb72db8e9d5554d1b4a948e182c748e
Mateus_Leepkaln <mateusleepklanjustino@gmail.com>
2021-09-09 16:51:47 -0300

    Create exercicio7b.py

---
+++ exercicio7b.py
@@ -0,0 +1,16 @@
+filmes = ["Divertidamente", "Alvin e os Esquilos", "Marley e Eu"]
+jogos = ["Terraria", "Roblox", "House Flipper"]                             #Letra A
+livros = ["Diário de Um Banana", "Jornada nas Estrelas", "Extraordinário"]
+esportes = ["Natação", "Corrida", "Futebol"]
+
+lista_principal = []
+lista_principal.append(filmes)
+lista_principal.append(jogos)                                               #Letra B
+lista_principal.append(livros)
+lista_principal.append(esportes)
+print("", lista_principal)
+
+print("Lista de Filmes: ", filmes)                                                #Letra C
+
+del esportes[2]
+print(esportes)                                                             #Letra D
