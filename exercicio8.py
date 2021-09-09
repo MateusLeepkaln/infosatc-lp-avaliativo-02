c5cdae6fe3eee0fb7c32bd070ef54f9c1d397652
Mateus_Leepkaln <mateusleepklanjustino@gmail.com>
2021-09-09 16:51:50 -0300

    Create exercicio8.py

---
+++ exercicio8.py
@@ -0,0 +1,6 @@
+pcgamer = ["Monitor 24'", "i9 9900K", "GTX 2080Ti", "Teclado Corsair MK2" "32GB RAM", "gabinete cooler master", "Placa Mãe Asus PRIME Z590M-PLUS"]
+print(pcgamer)
+del pcgamer[2]
+print(pcgamer)
+del pcgamer[5]
+print(pcgamer)
